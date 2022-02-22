f5da68fb7a2321b71775c301906b7cf5ba9251d9
algowifi <99904645+algowifi@users.noreply.github.com>
2022-02-22 16:25:04 +0100

    Update algoConfig.php

--- scriptsPHP/algoConfig.php
+++ scriptsPHP/algoConfig.php
@@ -2,7 +2,7 @@
     
     //1) Constants
     $mainAccountAddress = "HX2NYIIWEYBTSKE2EMAKIRZAQPRRL2JJOLPBCX33V2TYWGIBR626JHM6RA";
-    $mainWalletPw = 'shype2022';
+    $mainWalletPw = ''; //password wallet
     $mainWalletId = '27008546fcceca5252bf3938f21eff5e';
     $algodToken = '49f27db9b910c6e3548de27e73af38697394906ff2c2f0aeace2fe2b15589bce';
     $kmdToken = 'b1bdecba8a5374ea4e4b853df49f9d58c1877ee5edcbd2fba63653228dc35d74';
