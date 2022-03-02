dcf19999b00d4e863e0b6907e9b62f5f0a93231f
mdifiore <difiorem@gmail.com>
2022-03-02 17:17:25 +0100

    Add files via upload
    layout two columns for profile

--- profile.php
+++ profile.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<?php 
+<?php
 include 'check.php';
 include './sdk/algorand.php';
 include './scriptsPHP/algoConfig.php';
-    
+
 ?>
 <html lang="en">
+
 <head>
     <title>User Profile</title>
     <meta charset='utf-8'>
@@ -21,95 +22,110 @@ include './scriptsPHP/algoConfig.php';
     <link rel="stylesheet" href="./css/menu.css">
     <script type="text/javascript" src="./js/profile.js"></script>
 </head>
+
 <body id="body-pd" class="body-pd">
     <header class="header body-pd" id="header">
         <div class="header_toggle"> <i class='bx bx-menu bx-x' id="header-toggle"></i> </div>
         <div class="header_img"> <img src="./img/Alogo.png" alt=""> </div>
     </header>
-    
+
     <?php
-        printMenu();
+    printMenu();
     ?>
 
     <!--Container Main start-->
     <div class="height-100 bg-light" style="overflow:scroll;">
         <div id="liveAlertPlaceholder"></div>
-        <h4><?php if ($_SESSION['user']['isAdmin']) echo "Admin"; else echo "User";?> Profile</h4>
+        <h4><?php if ($_SESSION['user']['isAdmin']) echo "Admin";
+            else echo "User"; ?> Profile</h4>
         <div id="userInfoContainer">
-        <!-- <p>Scan to follow this account on Algorand mobile App</p>
-            <img src=<?php echo "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=".$_SESSION['user']['algorandAddress']; ?> title="Algorand Address QR-Code" />
+            <!-- <p>Scan to follow this account on Algorand mobile App</p>
+            <img src=<?php echo "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" . $_SESSION['user']['algorandAddress']; ?> title="Algorand Address QR-Code" />
         <br> -->
 
-            <h5>Personal Data</h5>
-        <?php
-            echo "<p>".$_SESSION['user']['name']."</p>";
-            echo "<p>".$_SESSION['user']['email']."</p>";
-            echo "<p>".$_SESSION['user']['address']."</p>";
-            echo "<p>".$_SESSION['user']['note']."</p>";
-            // if ($_SESSION['user']['isAdmin']) 
-            // {
-            //     //get algo balance and algowifi balance
-            //     $return=$algorand->get("v1","account",$mainAccountAddress);
-            //     $return_array=json_decode($return['response']);
-            //     $algoPlatformBalance=$return_array->{'amount'} / 1000000;
-            //     $algoWifiPlatformBalance=$return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000 ;
-            //     //print platform Data
-            //     echo "<hr>";
-            //     echo "<h5>Platform Data</h5>";
-            //     echo "<p>Scan to follow the platform account on Algorand mobile App</p><img src=";
-            //     echo "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=".$mainAccountAddress;
-            //     echo ' title="Algorand Address QR-Code" /><br>';
-        
-            //     //print platform account address
-            //     echo "<p><a target='_blank' href='".$algoExplorerUrlPrefix.$mainAccountAddress."'>".$mainAccountAddress."</a></p>";
-            //     //print algo and algowifi balance
-            //     echo "<p>Algo balance: ".number_format($algoPlatformBalance, 3, '.', ',')."</p>";
-            //     echo "<p>AWIFI balance: ".number_format($algoWifiPlatformBalance, 4, '.', ',')."</p>";
-            // } 
-            // else 
-            {
-                echo "<p>Scan to follow your account on Algorand mobile App</p><img src=";
-                echo "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=".$_SESSION['user']['algorandAddress'];
-                echo ' title="Algorand Address QR-Code" /><br>';
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h5>Personal Data</h5>
+                        <?php
+                        echo "<p>" . $_SESSION['user']['name'] . "</p>";
+                        echo "<p>" . $_SESSION['user']['email'] . "</p>";
+                        echo "<p>" . $_SESSION['user']['address'] . "</p>";
+                        echo "<p>" . $_SESSION['user']['note'] . "</p>";
+                        // if ($_SESSION['user']['isAdmin']) 
+                        // {
+                        //     //get algo balance and algowifi balance
+                        //     $return=$algorand->get("v1","account",$mainAccountAddress);
+                        //     $return_array=json_decode($return['response']);
+                        //     $algoPlatformBalance=$return_array->{'amount'} / 1000000;
+                        //     $algoWifiPlatformBalance=$return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000 ;
+                        //     //print platform Data
+                        //     echo "<hr>";
+                        //     echo "<h5>Platform Data</h5>";
+                        //     echo "<p>Scan to follow the platform account on Algorand mobile App</p><img src=";
+                        //     echo "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=".$mainAccountAddress;
+                        //     echo ' title="Algorand Address QR-Code" /><br>';
 
-                echo "<p><a target='_blank' href='".$algoExplorerUrlPrefix.$_SESSION['user']['algorandAddress']."'>".$_SESSION['user']['algorandAddress']."</a></p>";
+                        //     //print platform account address
+                        //     echo "<p><a target='_blank' href='".$algoExplorerUrlPrefix.$mainAccountAddress."'>".$mainAccountAddress."</a></p>";
+                        //     //print algo and algowifi balance
+                        //     echo "<p>Algo balance: ".number_format($algoPlatformBalance, 3, '.', ',')."</p>";
+                        //     echo "<p>AWIFI balance: ".number_format($algoWifiPlatformBalance, 4, '.', ',')."</p>";
+                        // } 
+                        // else 
+                        {
+                            echo "<p>Scan to follow your account on Algorand mobile App</p><img src=";
+                            echo "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" . $_SESSION['user']['algorandAddress'];
+                            echo ' title="Algorand Address QR-Code" /><br>';
 
-                //get algo balance and algowifi balance
-                $return=$algorand->get("v1","account",$_SESSION['user']['algorandAddress']);
-                $return_array=json_decode($return['response']);
-                $algoBalance=$return_array->{'amount'} / 1000000;
-                $algoWifiBalance=$return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000 ;
-                //print algo and algowifi balance
-                //echo "<p>Algo balance: ".number_format($algoBalance, 3, '.', ',')."</p>";
-                echo "<p>AWIFI balance: ".number_format($algoWifiBalance, 4, '.', ',')."</p>";
-            }
-        ?>
+                            echo "<p><a target='_blank' href='" . $algoExplorerUrlPrefix . $_SESSION['user']['algorandAddress'] . "'>" . $_SESSION['user']['algorandAddress'] . "</a></p>";
+
+                            //get algo balance and algowifi balance
+                            $return = $algorand->get("v1", "account", $_SESSION['user']['algorandAddress']);
+                            $return_array = json_decode($return['response']);
+                            $algoBalance = $return_array->{'amount'} / 1000000;
+                            $algoWifiBalance = $return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000;
+                            //print algo and algowifi balance
+                            //echo "<p>Algo balance: ".number_format($algoBalance, 3, '.', ',')."</p>";
+                            echo "<p>AWIFI balance: " . number_format($algoWifiBalance, 4, '.', ',') . "</p>";
+                        }
+                        ?>
+                    </div>
+                    <div class="col">
+                        <!-- Change Pw form -->
+                        <form id="changePwForm">
+                            <div class="modal-body">
+                                <h5>Change Password</h5>
+                                <div class="form-group">
+                                    <label>New password</label>
+                                    <input id="newPw" type="password" class="form-control" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Repeat password</label>
+                                    <input id="newPw2" type="password" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <div id="spinner" class="spinner-border text-primary" role="status" style="display: none;">
+                                    <span class="sr-only"></span>
+                                </div>
+                                <button type="button" id="btnCancel" class="btn btn-secondary" tabindex="2">Cancel</button>
+                                <button type="submit" id="btnSave" value="btnSave" class="btn btn-primary" translate="1">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
         </div>
+        <!-- end userinfocontainer -->
 
         <hr>
-        <!-- Change Pw form -->
-        <form id="changePwForm">
-                <div class="modal-body">
-                    <h5>Change Password</h5>
-                    <div class="form-group">
-                        <label>New password</label>
-                        <input id="newPw" type="password" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Repeat password</label>
-                        <input id="newPw2" type="password" class="form-control" required>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <div id="spinner" class="spinner-border text-primary" role="status" style="display: none;">
-                        <span class="sr-only"></span>
-                    </div>
-                    <button type="button" id="btnCancel" class="btn btn-secondary" tabindex="2">Cancel</button>
-                    <button type="submit" id="btnSave" value="btnSave" class="btn btn-primary" translate="1">Save</button>
-                </div>
-        </form>
+
 
     </div>
     <!--Container Main end-->
 </body>
+
 </html>
