c330541bb6ef3757234ca2168eab768c4873d319
mdifiore <difiorem@gmail.com>
2022-03-03 19:44:33 +0100

    Add files via upload
    default campaign make a transaction from reserve account to hotspotter

--- algowifi.php
+++ algowifi.php
@@ -7,7 +7,7 @@ include('./scriptsPHP/algoConfig.php');
 $performTransactions = true;
 $missingNFT = false;
 $defaultImage = "./img/default.jpeg";
-$defaultLandingPage = "'https://algowifi.com'";
+$defaultLandingPage = "https://algowifi.com";
 
 $mac = $_POST['mac'];
 $ip = $_POST['ip'];
@@ -21,7 +21,7 @@ $linkloginonly = $_POST['link-login-only'];
 $linkorigesc = $_POST['link-orig-esc']; // pagina di landing dopo accesso. Possibilita' di variarla in base alla campagna.
 $macesc = $_POST['mac-esc'];
 $server = $_POST['server-name'];
-$NFT_wifi = $_POST['identity']; 
+$NFT_wifi = $_POST['identity'];
 
 $referAdd = $_SERVER['HTTP_REFERER'];
 
@@ -58,56 +58,52 @@ if (!$missingNFT) {
     if ($result->num_rows != 1) {
         $image = $defaultImage;
         $linkorigesc = $defaultLandingPage;
-        $performAlgoFunctions = false;
-    }
-
-    $row = $result->fetch_assoc();
-    $message = $row['name'];
-    $validity = $row['isActive'];
-    $image = $row['imageUrl'];
-    $algoaddressADV = $row['publisher_address'];
-    $publisherId = $row['publisher_id'];
-    $relationId = $row['relation_id'];
-    $linkorigesc = $row['landingUrl'];
-    $id_campaign = $row['id'];
-    // $combined = "Mac : $mac - Location  : $server - $message"; // Note to blockchain
-
-    //build a json note for blockchain
-    $output['Mac'] = $mac;
-    $output['Location'] = $server. " - ".$message;
-    $output['CampaignName'] = $message;
-    $output['CampaignId'] = $row['id'];
-    $output['referer'] = $referAdd;
-    $combined = json_encode($output);
-
-    //get Publisher AWIFI balance
-    $return = $algorand->get("v1", "account", $algoaddressADV);
-    $return_array = json_decode($return['response']);
-    $AWIFI = $return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000;
-
-    //if balance == 0 show default campaign and disable every campaign of this publisher 
-    if ($AWIFI == 0) 
-    {
-        $image = $defaultImage;
-        $linkorigesc = $defaultLandingPage;
         $performTransactions = false;
-        
-        // Disable every campaign of this publisher
-        $sql2 = "UPDATE Campaign SET isActive=0 WHERE userId=".$publisherId;
-        error_log("performing query ".$sql2, 0);
-        if ($conn->query($sql2) === TRUE) 
-        {
-            error_log("Publisher ".$algoaddressADV." has balance 0! Every campaign has been disabled!");
-        } else 
-        {
-            error_log("Error: " . $sql2 . " " . $conn->error);
+    } else {
+        $row = $result->fetch_assoc();
+        $message = $row['name'];
+        $validity = $row['isActive'];
+        $image = $row['imageUrl'];
+        $algoaddressADV = $row['publisher_address'];
+        $publisherId = $row['publisher_id'];
+        $relationId = $row['relation_id'];
+        $linkorigesc = $row['landingUrl'];
+        $id_campaign = $row['id'];
+        // $combined = "Mac : $mac - Location  : $server - $message"; // Note to blockchain
+
+        //build a json note
+        $output['Mac'] = $mac;
+        $output['Location'] = $server . " - " . $message;
+        $output['CampaignName'] = $message;
+        $output['CampaignId'] = $row['id'];
+        $output['referer'] = $referAdd;
+        $combined = json_encode($output);
+
+        //get Publisher AWIFI balance
+        $return = $algorand->get("v1", "account", $algoaddressADV);
+        $return_array = json_decode($return['response']);
+        $AWIFI = $return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000;
+
+        //if balance == 0 show default campaign and disable every campaign of this publisher 
+        if ($AWIFI == 0) {
+            $image = $defaultImage;
+            $linkorigesc = $defaultLandingPage;
+            $performTransactions = false;
+
+            // Disable every campaign of this publisher
+            $sql2 = "UPDATE Campaign SET isActive=0 WHERE userId=" . $publisherId;
+            error_log("performing query " . $sql2, 0);
+            if ($conn->query($sql2) === TRUE) {
+                error_log("Publisher " . $algoaddressADV . " has balance 0! Every campaign has been disabled!");
+            } else {
+                error_log("Error: " . $sql2 . " " . $conn->error);
+            }
         }
     }
 } //end !missingNFT
 
 
-if ($performTransactions)
- {
+if ($performTransactions) {
 
     #Prepare transaction
     // get lastRound from algod
@@ -117,8 +113,8 @@ if ($performTransactions)
 
     //3) make a transaction Group
     //Transaction 1 : payment to platform
-    $transactions=array();
-    $transactions[]=array(
+    $transactions = array();
+    $transactions[] = array(
         "txn" => array(
             "aamt" => $platformFee,
             "type" => "axfer", //Tx Type
@@ -134,7 +130,7 @@ if ($performTransactions)
         ),
     );
     //Transaction 2 : payment to hotspot owner
-    $transactions[]=array(
+    $transactions[] = array(
         "txn" => array(
             "aamt" => $hotspotFee,
             "type" => "axfer", //Tx Type
@@ -149,9 +145,9 @@ if ($performTransactions)
             "xaid" => $algowifiAssetId, // ID Asa
         ),
     );
-    
+
     //Transaction 3 : payment to reserve address
-    $transactions[]=array(
+    $transactions[] = array(
         "txn" => array(
             "aamt" => $reserveFee,
             "type" => "axfer", //Tx Type
@@ -166,90 +162,129 @@ if ($performTransactions)
             "xaid" => $algowifiAssetId, // ID Asa
         ),
     );
-    
-    
+
+
     //2) Group TRansactions
-    $groupid=$algorand_kmd->groupid($transactions);
+    $groupid = $algorand_kmd->groupid($transactions);
     #Assigns Group ID
-    $transactions[0]['txn']['grp']=$groupid;
-    $transactions[1]['txn']['grp']=$groupid;
-    $transactions[2]['txn']['grp']=$groupid; 
-    
-    
-    
+    $transactions[0]['txn']['grp'] = $groupid;
+    $transactions[1]['txn']['grp'] = $groupid;
+    $transactions[2]['txn']['grp'] = $groupid;
+
+
+
     //3) Sign Transactions
     #Sign Transaction 1
-    $txn="";
-    $clearTxn="";
-    $params['params']=array(
-    //"public_key" => $algorand_kmd->pk_encode($mainAccountAddress),
-    "transaction" => $algorand_kmd->txn_encode($transactions[0]),
-    "wallet_handle_token" => $wallet_handle_token,
-    "wallet_password" => $mainWalletPw,
+    $txn = "";
+    $clearTxn = "";
+    $params['params'] = array(
+        //"public_key" => $algorand_kmd->pk_encode($mainAccountAddress),
+        "transaction" => $algorand_kmd->txn_encode($transactions[0]),
+        "wallet_handle_token" => $wallet_handle_token,
+        "wallet_password" => $mainWalletPw,
     );
 
 
-    $return=$algorand_kmd->post("v1","transaction","sign",$params);
-    $r=json_decode($return['response']);
-    $txn.=base64_decode($r->signed_transaction);
-    $clearTxn.=$r->signed_transaction;
+    $return = $algorand_kmd->post("v1", "transaction", "sign", $params);
+    $r = json_decode($return['response']);
+    $txn .= base64_decode($r->signed_transaction);
+    $clearTxn .= $r->signed_transaction;
 
     #Sign Transaction 2
-    $params['params']=array(
-    //"public_key" => $algorand_kmd->pk_encode($mainAccountAddress),
-    "transaction" => $algorand_kmd->txn_encode($transactions[1]),
-    "wallet_handle_token" => $wallet_handle_token,
-    "wallet_password" => $mainWalletPw,
+    $params['params'] = array(
+        //"public_key" => $algorand_kmd->pk_encode($mainAccountAddress),
+        "transaction" => $algorand_kmd->txn_encode($transactions[1]),
+        "wallet_handle_token" => $wallet_handle_token,
+        "wallet_password" => $mainWalletPw,
     );
-    
-    $return=$algorand_kmd->post("v1","transaction","sign",$params);
-    $r=json_decode($return['response']);
-    $txn.=base64_decode($r->signed_transaction);
-    $clearTxn.=$r->signed_transaction;
-    
-     #Sign Transaction 3
-    $params['params']=array(
-    //"public_key" => $algorand_kmd->pk_encode($mainAccountAddress),
-    "transaction" => $algorand_kmd->txn_encode($transactions[2]),
-    "wallet_handle_token" => $wallet_handle_token,
-    "wallet_password" => $mainWalletPw,
+
+    $return = $algorand_kmd->post("v1", "transaction", "sign", $params);
+    $r = json_decode($return['response']);
+    $txn .= base64_decode($r->signed_transaction);
+    $clearTxn .= $r->signed_transaction;
+
+    #Sign Transaction 3
+    $params['params'] = array(
+        //"public_key" => $algorand_kmd->pk_encode($mainAccountAddress),
+        "transaction" => $algorand_kmd->txn_encode($transactions[2]),
+        "wallet_handle_token" => $wallet_handle_token,
+        "wallet_password" => $mainWalletPw,
     );
-    
-    $return=$algorand_kmd->post("v1","transaction","sign",$params);
-    $r=json_decode($return['response']);
-    $txn.=base64_decode($r->signed_transaction);
-    $clearTxn.=$r->signed_transaction;   
+
+    $return = $algorand_kmd->post("v1", "transaction", "sign", $params);
+    $r = json_decode($return['response']);
+    $txn .= base64_decode($r->signed_transaction);
+    $clearTxn .= $r->signed_transaction;
 
     //4) Send Transaction Group
     #Broadcasts a raw atomic transaction to the network.
-    $params['transaction']=$txn;
-    $return=$algorand->post("v2","transactions",$params);
-    $txId=$return['response']->txId;
+    $params['transaction'] = $txn;
+    $return = $algorand->post("v2", "transactions", $params);
+    $txId = $return['response']->txId;
 
-   //5) check transaction status
-   $return_array=json_decode($return['response']);
-   $transactionID=$return_array->{'txId'} ;
-   if ($return['code'] == 200)
-   {
+    //5) check transaction status
+    $return_array = json_decode($return['response']);
+    $transactionID = $return_array->{'txId'};
+    if ($return['code'] == 200) {
         //Increment views for relation hotspot_campaign
-        $sql3 = "UPDATE Hotspot_Campaign SET views = views + 1 WHERE id = ".$relationId;
-        error_log("performing query ".$sql3, 0);
-        if ($conn->query($sql3) === TRUE) 
-        {
-            error_log("Certified view for campaign ".$id_campaign);
-        } else 
-        {
+        $sql3 = "UPDATE Hotspot_Campaign SET views = views + 1 WHERE id = " . $relationId;
+        error_log("performing query " . $sql3, 0);
+        if ($conn->query($sql3) === TRUE) {
+            error_log("Certified view for campaign " . $id_campaign);
+        } else {
             error_log("Error: " . $sql3 . " " . $conn->error);
         }
-   }
-   else
-   {
-       $s = print_r($return, true);
-       $failString = 'Transaction Failed! '.$s;
-       error_log($failString);
-   }
-    
+    } else {
+        $s = print_r($return, true);
+        $failString = 'Transaction Failed! ' . $s;
+        error_log($failString);
+    }
 } //end performTransactions
+else if ($image == $defaultImage && !$missingNFT) {
+
+    //build a json note
+    $output['Mac'] = $mac;
+    $output['Location'] = $server . " - " . "defaultCampaign";
+    $output['CampaignName'] = "defaultCampaign";
+    $output['CampaignId'] = "defaultCampaign";
+    $output['referer'] = $referAdd;
+    $combined = json_encode($output);
+    
+    // get lastRound from algod
+    $return = $algorand->get("v2", "status");
+    $return_array = json_decode($return['response']);
+    $lastround = $return_array->{'last-round'};
+
+    //perform reserve account to hotspotter transaction
+    $transaction = array(
+        "txn" => array(
+            "aamt" => $reserveFee,
+            "type" => "axfer", //Tx Type
+            "fee" => 1000, //Fee
+            "fv" => $lastround, //Take the last round
+            "gen" => $genesisID, // GenesisID
+            "gh" => $genesis, //Genesis Hash
+            "lv" => $lastround + 200, //Add 200 round 
+            "note" => $combined, //Your note
+            "snd" => $mainReserveAddress, //Sender platform
+            "arcv" => $nftOwnerAddress, //Receiver reserve address
+            "xaid" => $algowifiAssetId, // ID Asa
+        ),
+    );
+    //sign transaction
+    $params['params'] = array(
+        "transaction" => $algorand_kmd->txn_encode($transaction),
+        "wallet_handle_token" => $wallet_handle_token,
+        "wallet_password" => $mainWalletPw,
+    );
+    $return = $algorand_kmd->post("v1", "transaction", "sign", $params);
+    $r = json_decode($return['response']);
+    $txn = base64_decode($r->signed_transaction);
+
+    //broadcast transaction
+    $params['transaction'] = $txn;
+    $return = $algorand->post("v2", "transactions", $params);
+}
 
 $conn->close();
 
@@ -268,13 +303,11 @@ $conn->close();
     <meta http-equiv="expires" content="-1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style type="text/css">
-        
-
-        #container{
-            margin-top: 10%;   
-            text-align: center;   
-            color: #c1c1c1; 
-            font-size: 30px;  
+        #container {
+            margin-top: 10%;
+            text-align: center;
+            color: #c1c1c1;
+            font-size: 30px;
             font-family: verdana;
         }
 
@@ -283,9 +316,10 @@ $conn->close();
         }
 
         #connectUrl {
-            color: #FF8080 ;
+            color: #FF8080;
             font-size: 32px;
         }
+
         a,
         a:link,
         a:visited,
@@ -294,29 +328,30 @@ $conn->close();
             text-decoration: none;
             font-size: 10px;
         }
+
         a:hover {
             border-bottom: 1px dotted #c1c1c1;
             color: #AAAAAA;
         }
+
         #errorDiv {
-            color: #FF8080; 
+            color: #FF8080;
             font-size: 9px;
         }
-
     </style>
 
 </head>
 
 <body>
 
-<div id="container">
-    <p>Questa connessione e' offerta da:</p>
-    <img src="<?= $image; ?>">
-    <p>
-        Per collegarti, <a id="connectUrl" href="<?php echo $linkloginonly; ?>?dst=<?php echo $linkorigesc; ?>&username=T-<?php echo $macesc; ?>">click here</a>.
-    </p>
-    <div id="errorDiv"><?php echo $error; ?></div> 
-</div>
+    <div id="container">
+        <p>Questa connessione e' offerta da:</p>
+        <img src="<?= $image; ?>">
+        <p>
+            Per collegarti, <a id="connectUrl" href="<?php echo $linkloginonly; ?>?dst=<?php echo $linkorigesc; ?>&username=T-<?php echo $macesc; ?>">click here</a>.
+        </p>
+        <div id="errorDiv"><?php echo $error; ?></div>
+    </div>
 
 </body>
 
