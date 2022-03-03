545ba4a208ba0eb0ee4d07a9b2fdd02336cab0a4
mdifiore <difiorem@gmail.com>
2022-03-03 21:40:08 +0100

    Add files via upload

--- algowifiRest.php
+++ algowifiRest.php
@@ -4,28 +4,28 @@ include './scriptsPHP/dbConn.php';
 include('sdk/algorand.php');
 include('./scriptsPHP/algoConfig.php');
 
-header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 
 $performTransactions = true;
 $missingNFT = false;
 $defaultImage = "./img/default.jpeg";
 $defaultLandingPage = "'https://algowifi.com'";
 
-$mac = $_POST['mac'];
-$ip = $_POST['ip'];
-$username = $_POST['username'];
-$linklogin = $_POST['link-login'];
-$linkorig = $_POST['link-orig'];
-$error = $_POST['error'];
-$chapid = $_POST['chap-id'];
-$chapchallenge = $_POST['chap-challenge'];
-$linkloginonly = $_POST['link-login-only'];
-$linkorigesc = $_POST['link-orig-esc']; // pagina di landing dopo accesso. Possibilita' di variarla in base alla campagna.
-$macesc = $_POST['mac-esc'];
-$server = $_POST['server-name'];
+// $mac = $_POST['mac'];
+// $ip = $_POST['ip'];
+// $username = $_POST['username'];
+// $linklogin = $_POST['link-login'];
+// $linkorig = $_POST['link-orig'];
+// $error = $_POST['error'];
+// $chapid = $_POST['chap-id'];
+// $chapchallenge = $_POST['chap-challenge'];
+// $linkloginonly = $_POST['link-login-only'];
+// $linkorigesc = $_POST['link-orig-esc']; // pagina di landing dopo accesso. Possibilita' di variarla in base alla campagna.
+// $macesc = $_POST['mac-esc'];
+// $server = $_POST['server-name'];
 $NFT_wifi = $_POST['identity']; 
-
 $referAdd = $_SERVER['HTTP_REFERER'];
+$remoteAdd = $_SERVER['REMOTE_ADDR'];
 
 //se l'nft non è impostato, mostra campagna di default
 if (!isset($NFT_wifi)) {
@@ -45,69 +45,87 @@ if (!$missingNFT) {
     $response = json_decode($response);
     $nftOwnerAddress = $response->{'balances'}[0]->{'address'};
 
-    //get hotspot mysql id
-    $sql1 = "SELECT id FROM Hotspot WHERE nft = " . $NFT_wifi . "";
-    $result = $conn->query($sql1);
+//get hotspot mysql id
+$sql1 = "SELECT id FROM Hotspot WHERE nft = " . $NFT_wifi . " AND validator = '" . $referAdd . "'";
+$result = $conn->query($sql1);
+$hotspotMysqlId = -1;
+if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
     $hotspotMysqlId = $row['id'];
+}
+//check if hotspot id is valid!!
+if ($hotspotMysqlId == -1) {
+    //set default campaign
+    $image = $defaultImage;
+    $linkorigesc = $defaultLandingPage;
+    $performTransactions = false;
+    $missingNFT = true;
+    error_log("not found any hotspot for nft: ".$NFT_wifi." && validator: ".$referAdd);
+} 
+else 
+{
+     //Get active Campaign
+     $sql = "SELECT C.*, HC.id as relation_id, HC.hotspotId, HC.campaignId, U.algorandAddress as publisher_address, U.id as publisher_id FROM User as U, Campaign as C, Hotspot_Campaign as HC WHERE C.isActive = 1 AND U.id = C.userId AND HC.hotspotId = " . $hotspotMysqlId . " AND C.id = HC.campaignId ORDER BY C.creation ASC LIMIT 1"; // carica la campagna attiva LiFo
+     error_log("performing query: ".$sql);
 
-    //Get active Campaign
-    $sql = "SELECT C.*, HC.id as relation_id, HC.hotspotId, HC.campaignId, U.algorandAddress as publisher_address, U.id as publisher_id FROM User as U, Campaign as C, Hotspot_Campaign as HC WHERE C.isActive = 1 AND U.id = C.userId AND HC.hotspotId = " . $hotspotMysqlId . " AND C.id = HC.campaignId ORDER BY C.creation ASC LIMIT 1"; // carica la campagna attiva LiFo
-
-    $result = $conn->query($sql);
-
-    //se non esiste una campagna attiva su questo hotspot, mostra default
-    if ($result->num_rows != 1) {
-        $image = $defaultImage;
-        $linkorigesc = $defaultLandingPage;
-        $performAlgoFunctions = false;
-    }
-    else 
-    {
-        $row = $result->fetch_assoc();
-        $image = $row['imageUrl'];
-    }
-
-    $message = $row['name'];
-    $validity = $row['isActive'];
-    $algoaddressADV = $row['publisher_address'];
-    $publisherId = $row['publisher_id'];
-    $relationId = $row['relation_id'];
-    $linkorigesc = $row['landingUrl'];
-    $id_campaign = $row['id'];
-    $combined = "Mac : $mac - Location  : $server - $message"; // Note to blockchain
-
-    //build a json note
-    $output['Mac'] = $mac;
-    $output['Location'] = $server. " - ".$message;
-    $output['CampaignName'] = $message;
-    $output['CampaignId'] = $row['id'];
-    $output['referer'] = $referAdd;
-    $combined = json_encode($output);
-
-    //get Publisher AWIFI balance
-    $return = $algorand->get("v1", "account", $algoaddressADV);
-    $return_array = json_decode($return['response']);
-    $AWIFI = $return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000;
-
-    //if balance == 0 show default campaign and disable every campaign of this publisher 
-    if ($AWIFI == 0) 
-    {
-        $image = $defaultImage;
-        $linkorigesc = $defaultLandingPage;
-        $performTransactions = false;
-        
-        // Disable every campaign of this publisher
-        $sql2 = "UPDATE Campaign SET isActive=0 WHERE userId=".$publisherId;
-        error_log("performing query ".$sql2, 0);
-        if ($conn->query($sql2) === TRUE) 
-        {
-            error_log("Publisher ".$algoaddressADV." has balance 0! Every campaign has been disabled!");
-        } else 
-        {
-            error_log("Error: " . $sql2 . " " . $conn->error);
-        }
-    }
+     $result = $conn->query($sql);
+ 
+     //se non esiste una campagna attiva su questo hotspot, mostra default
+     if ($result->num_rows != 1) {
+         $image = $defaultImage;
+         $linkorigesc = $defaultLandingPage;
+         $performTransactions = false;
+     }
+     else 
+     {
+         $row = $result->fetch_assoc();
+         $image = $row['imageUrl'];
+         $message = $row['name'];
+     $validity = $row['isActive'];
+     $algoaddressADV = $row['publisher_address'];
+     $publisherId = $row['publisher_id'];
+     $relationId = $row['relation_id'];
+     $linkorigesc = $row['landingUrl'];
+     $id_campaign = $row['id'];
+     //$combined = "Mac : $mac - Location  : $server - $message"; // Note to blockchain
+ 
+     //build a json note
+     //$output['Mac'] = $mac;
+     //$output['Location'] = $server. " - ".$message;
+     $output['CampaignName'] = $message;
+     $output['CampaignId'] = $row['id'];
+     $output['referer'] = $referAdd;
+     $output['remote'] = $remoteAdd;
+     $combined = json_encode($output);
+ 
+     //get Publisher AWIFI balance
+     $return = $algorand->get("v1", "account", $algoaddressADV);
+     $return_array = json_decode($return['response']);
+     $AWIFI = $return_array->{'assets'}->{$algowifiAssetId}->{'amount'} / 10000;
+ 
+     //if balance == 0 show default campaign and disable every campaign of this publisher 
+     if ($AWIFI == 0) 
+     {
+         $image = $defaultImage;
+         $linkorigesc = $defaultLandingPage;
+         $performTransactions = false;
+         
+         // Disable every campaign of this publisher
+         $sql2 = "UPDATE Campaign SET isActive=0 WHERE userId=".$publisherId;
+         error_log("performing query ".$sql2, 0);
+         if ($conn->query($sql2) === TRUE) 
+         {
+             error_log("Publisher ".$algoaddressADV." has balance 0! Every campaign has been disabled!");
+         } else 
+         {
+             error_log("Error: " . $sql2 . " " . $conn->error);
+         }
+     }
+     }
+ 
+     
+}
+   
 } //end !missingNFT
 
 
@@ -154,12 +172,30 @@ if ($performTransactions)
             "xaid" => $algowifiAssetId, // ID Asa
         ),
     );
+
+    //Transaction 3 : payment to reserve address
+    $transactions[] = array(
+        "txn" => array(
+            "aamt" => $reserveFee,
+            "type" => "axfer", //Tx Type
+            "fee" => 1000, //Fee
+            "fv" => $lastround, //Take the last round
+            "gen" => $genesisID, // GenesisID
+            "gh" => $genesis, //Genesis Hash
+            "lv" => $lastround + 200, //Add 200 round 
+            "note" => $combined, //Your note
+            "snd" => $mainAccountAddress, //Sender platform
+            "arcv" => $mainReserveAddress, //Receiver reserve address
+            "xaid" => $algowifiAssetId, // ID Asa
+        ),
+    );
     
     //2) Group TRansactions
     $groupid=$algorand_kmd->groupid($transactions);
     #Assigns Group ID
     $transactions[0]['txn']['grp']=$groupid;
     $transactions[1]['txn']['grp']=$groupid;
+    $transactions[2]['txn']['grp']=$groupid;
  
     //3) Sign Transactions
     #Sign Transaction 1
@@ -191,6 +227,19 @@ if ($performTransactions)
     $txn.=base64_decode($r->signed_transaction);
     $clearTxn.=$r->signed_transaction;
 
+    #Sign Transaction 3
+    $params['params'] = array(
+        //"public_key" => $algorand_kmd->pk_encode($mainAccountAddress),
+        "transaction" => $algorand_kmd->txn_encode($transactions[2]),
+        "wallet_handle_token" => $wallet_handle_token,
+        "wallet_password" => $mainWalletPw,
+    );
+
+    $return = $algorand_kmd->post("v1", "transaction", "sign", $params);
+    $r = json_decode($return['response']);
+    $txn .= base64_decode($r->signed_transaction);
+    $clearTxn .= $r->signed_transaction;
+
     //4) Send Transaction Group
     #Broadcasts a raw atomic transaction to the network.
     $params['transaction']=$txn;
@@ -221,13 +270,63 @@ if ($performTransactions)
    }
     
 } //end performTransactions
+else if ($image == $defaultImage && !$missingNFT) {
+
+    //build a json note
+    //$output['Mac'] = $mac;
+    //$output['Location'] = $server . " - " . "defaultCampaign";
+    $output['CampaignName'] = "defaultCampaign";
+    $output['CampaignId'] = "defaultCampaign";
+    $output['referer'] = $referAdd;
+    $output['remote'] = $remoteAdd;
+    $combined = json_encode($output);
+    
+    // get lastRound from algod
+    $return = $algorand->get("v2", "status");
+    $return_array = json_decode($return['response']);
+    $lastround = $return_array->{'last-round'};
+
+    //perform reserve account to hotspotter transaction
+    $transaction = array(
+        "txn" => array(
+            "aamt" => $reserveFee,
+            "type" => "axfer", //Tx Type
+            "fee" => 1000, //Fee
+            "fv" => $lastround, //Take the last round
+            "gen" => $genesisID, // GenesisID
+            "gh" => $genesis, //Genesis Hash
+            "lv" => $lastround + 200, //Add 200 round 
+            "note" => $combined, //Your note
+            "snd" => $mainReserveAddress, //Sender platform
+            "arcv" => $nftOwnerAddress, //Receiver reserve address
+            "xaid" => $algowifiAssetId, // ID Asa
+        ),
+    );
+    //sign transaction
+    $params['params'] = array(
+        "transaction" => $algorand_kmd->txn_encode($transaction),
+        "wallet_handle_token" => $wallet_handle_token,
+        "wallet_password" => $mainWalletPw,
+    );
+    $return = $algorand_kmd->post("v1", "transaction", "sign", $params);
+    $r = json_decode($return['response']);
+    $txn = base64_decode($r->signed_transaction);
+
+    //broadcast transaction
+    $params['transaction'] = $txn;
+    $return = $algorand->post("v2", "transactions", $params);
+}
 
 $conn->close();
 
-$fullImageUrl = str_replace("./img/", "https://algowifi.com/algowifi/img/", $image);
+if ($image == $defaultImage)
+{
+    $fullImageUrl = str_replace("./img/", "https://algowifi.com/algowifi/img/", $image);
+}
+else
+{
+    $fullImageUrl = str_replace("./", "https://algowifi.com/algowifi/", $image);
+}
 
-echo $fullImageUrl
-
-?>
-
-
+$output['image'] = $fullImageUrl;
+echo json_encode($output);
