514b69bd088bc1ddc5cab8ba27c88f0a5e75891e
mdifiore <difiorem@gmail.com>
2022-07-28 17:59:55 +0200

    Update algoConfig.php
    added central bak account

--- scriptsPHP/algoConfig.php
+++ scriptsPHP/algoConfig.php
@@ -3,6 +3,7 @@
     //1) Constants
     $mainAccountAddress = "HX2NYIIWEYBTSKE2EMAKIRZAQPRRL2JJOLPBCX33V2TYWGIBR626JHM6RA";
     $mainReserveAddress = "QYEZ6NCSPFSU53WWEOROREHFBYP42FBSUSJ5ZEM3R2R5VPNUSNKDHG5JSY"; //reserve address 
+    $centralBankAddress = "TI2UFIE3VEAHHQZPQIPFR6ISJP66P452YKJISYUWJMEYHODX5INJQ3465Q"; 
     $mainWalletPw = ''; //password wallet
     $mainWalletId = '27008546fcceca5252bf3938f21eff5e';
     $algodToken = '49f27db9b910c6e3548de27e73af38697394906ff2c2f0aeace2fe2b15589bce';
